238e7b0c92c1a5fbd5fa4af9eef261fcf798d63f
hendryriq <hendryriq@gmail.com>
2026-02-10 21:10:38 +0700

    fix(auth): update login error message

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -25,7 +25,7 @@ class AuthController extends Controller
 
         if (!$user || !Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
-                'email' => ['The provided credentials are incorrect.'],
+                'email' => ['Email or password is wrong.'],
             ]);
         }
 
