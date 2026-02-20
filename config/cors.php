172f174d06174b37c753d290d701eda5d2bc1114
hendryriq <hendryriq@gmail.com>
2026-02-20 14:12:26 +0700

    feat: Replace explicit localhost origins with regex patterns in CORS configuration.

--- config/cors.php
+++ config/cors.php
@@ -19,14 +19,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        'http://localhost:5175',
-        'http://127.0.0.1:5175',
-    ],
+    'allowed_origins' => [],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '#^http://localhost(:\d+)?$#',
+        '#^http://127\.0\.0\.1(:\d+)?$#',
+    ],
 
     'allowed_headers' => ['*'],
 
